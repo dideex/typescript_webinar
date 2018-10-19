0eba54f38ef7e99500f4389caa41fd92fce7d72d
DiDex <wc34fun@mail.ru>
2018-10-19 11:14:58 +0800

    rework cashier

--- exercises/cashier/src/cashier.ts
+++ exercises/cashier/src/cashier.ts
@@ -1,24 +1,39 @@
 // export function cashier() {
 //   return {};
 // }
-interface Cart {
+interface CartItem {
   name: string;
   price: number;
   qty: number;
 }
 
-export class Cashier {
-  cart: Cart[] = [];
-  add(name: string, price: number, qty: number = 1): void {
-    this.cart.push({ name, price, qty });
-  }
-  addItem(item: Cart): void {
-    this.cart.push(item);
-  }
-  get length(): number {
-    return this.cart.reduce((quantity, { qty }) => (quantity + qty), 0);
-  }
-  get total(): number {
-    return this.cart.reduce((totalPrice, { price, qty }) => (totalPrice + (price * qty)), 0);
-  }
+interface CartAPI {
+  length: number;
+  total: number;
+  addItem(item: CartItem): CartAPI;
+  add(name: string, price: number, qty: number): CartAPI;
 }
+
+// export class Cashier {
+export function cashier(): CartAPI {
+  let cart: CartItem[] = [];
+  return {
+    add(name: string, price: number, qty: number = 1): CartAPI {
+      cart.push({ name, price, qty });
+      return this;
+    },
+    addItem(item: CartItem): CartAPI {
+      cart.push(item);
+      return this;
+    },
+    get length(): number {
+      return cart.reduce((quantity, { qty }) => quantity + qty, 0);
+    },
+    get total(): number {
+      return cart.reduce((totalPrice, { price, qty }) => totalPrice + price * qty, 0);
+    }
+  };
+}
+console.log(
+  cashier().add('name',  12, 1).add('name',  12, 5).total
+)
